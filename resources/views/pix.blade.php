994885002a6d324bdf4c10968974f6e2117b634c
Luciano Eduardo <luciano123ab@gmail.com>
2026-01-30 21:01:27 -0300

    009(Historico)

--- resources/views/pix.blade.php
+++ resources/views/pix.blade.php
@@ -5,7 +5,7 @@
         <h5 class="card-header text-center">QRCode</h5>
         <img src="{{ $qrcode }}">
         <div class="card-body">
-            <p>Após realizar o pagamento, envie o comprovante pelo nosso whatsapp que se encontra na barra inferior da página.</p>
+            <p>Após realizar o pagamento, envie o comprovante pelo nosso whatsapp que se encontra na barra inferior da página, informando seu nome de usuário.</p>
         </div>
     </div>
 @endsection
