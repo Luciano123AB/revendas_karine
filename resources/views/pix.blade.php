3f713bf94d899c931e410113227f2ea0942c3194
Luciano Eduardo <luciano123ab@gmail.com>
2026-02-05 21:26:45 -0300

    014(Pesquisa_Correcoes)

--- resources/views/pix.blade.php
+++ resources/views/pix.blade.php
@@ -3,9 +3,10 @@
 @section("content")
     <div class="fundo card" id="qrcode">
         <h5 class="card-header text-center">QRCode</h5>
-        <img src="{{ $qrcode }}">
+        <img src="{{ $qrcode }}" class="card-img-top">
         <div class="card-body">
-            <p>Após realizar o pagamento, envie o comprovante pelo nosso whatsapp que se encontra na barra inferior da página, informando seu nome de usuário.</p>
+            <p class="card-text">Após realizar o pagamento, envie o comprovante pelo nosso whatsapp que se encontra na barra inferior da página, informando seu nome de usuário.</p>
         </div>
+        <div class="card-footer"></div>
     </div>
 @endsection
