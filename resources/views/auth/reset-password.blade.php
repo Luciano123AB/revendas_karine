7da8c0216134710d6345a8e4d32fab14e916c426
Luciano Eduardo <luciano123ab@gmail.com>
2026-02-05 22:39:37 -0300

    014(Pesquisa_Correcoes)

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -13,7 +13,10 @@
 
             <div class="form-group">
                 <label><i class="bi bi-key"></i> Senha:</label>
-                <input type="password" class="form-control focus-ring focus-ring-danger" name="password" placeholder="***">
+                <div class="input-group">
+                    <input type="password" id="senha" class="form-control focus-ring focus-ring-danger" name="password" placeholder="***">
+                    <button type="button" id="exibir_ocultar" class="btn btn-light border-start"><i id="botao" class="bi bi-eye"></i></button>
+                </div>
                 @error('password')
                     <span class="text-danger">{{ $message }}</span>
                 @enderror
