0bdf49038d4410d4ab5a15dab540a3d6f07ffaf8
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2026-06-07 17:24:06 -0300

    Update VerificacaoEmail.php

--- app/Notifications/VerificacaoEmail.php
+++ app/Notifications/VerificacaoEmail.php
@@ -3,16 +3,12 @@
 namespace App\Notifications;
 
 use Carbon\Carbon;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\URL;
 
 class VerificacaoEmail extends Notification
 {
-    use Queueable;
-
     /**
      * Create a new notification instance.
      */
